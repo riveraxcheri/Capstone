cedae3fcb836e50bec4ecdbf8f88d2729ca325e1
riveraxcheri <117052183+riveraxcheri@users.noreply.github.com>
2023-07-30 12:42:33 -0500

    Fixed class to className

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -4,7 +4,7 @@ import blankImage from '../../assests/images/blank_image.png'
 const ProductCard = () => {
     return ( 
         <div className="card" style={{width: "18rem"}}>
-            <img src={blankImage} class="card-img-top" alt="blank"/>
+            <img src={blankImage} className="card-img-top" alt="blank"/>
             <div className="card-body">
                 <h3 className="card-title">Product Name</h3>
                 <p className="card-category">category</p>
